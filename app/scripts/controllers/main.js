f8491ec6b45a0edec5a80dff42b96df64479c447
Amitai Burstein <amitai@gizra.com>
2013-09-08 20:49:54 +0300

    First commit.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularjsOauthApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
