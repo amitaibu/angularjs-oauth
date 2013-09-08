b30a5f0da6efe6796cb6ff58955f236b3bc47006
Amitai Burstein <amitai@gizra.com>
2013-09-08 22:17:22 +0300

    Get user info.

--- app/scripts/directives/githubLogin.js
+++ app/scripts/directives/githubLogin.js
@@ -1,19 +1,26 @@
 'use strict';
 
 angular.module('angularjsOauthApp')
-  .directive('githubLogin', function () {
+  .directive('githubLogin', function ($http) {
     return {
       template: '<a href="#">Login</a>',
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
         OAuth.initialize('Wwa6lTxxmeeJxHIXLrGtu5bp6kw');
-        
+
+        // @todo: Get state from service.
+        var csrfToken = 1234;
+
         element.bind('click', function() {
-          //Using popup (option 1)
-          OAuth.popup('github', function(error, result) {
-            //handle error with error
-            //use result.access_token in your API request
+          OAuth.popup('github', {state: csrfToken}, function(error, result) {
             console.log(result);
+
+            $http({method: 'GET', url: 'https://oauth.io/api/me'}).
+              success(function(data, status, headers, config) {
+
+              }).
+              error(function(data, status, headers, config) {
+              });
           });
         });
       }
