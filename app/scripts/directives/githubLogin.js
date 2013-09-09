7b63c4152949155cfab001def18d02eef8cb17e2
Amitaibu <amitai@gizra.com>
2013-09-09 13:32:42 +0300

    Add todo.

--- app/scripts/directives/githubLogin.js
+++ app/scripts/directives/githubLogin.js
@@ -7,6 +7,8 @@ angular.module('angularjsOauthApp')
       restrict: 'E',
       link: function postLink(scope, element) {
         var OAuth = window.OAuth;
+
+        // @todo: Key should be taken from config.
         OAuth.initialize('Wwa6lTxxmeeJxHIXLrGtu5bp6kw');
 
         // @todo: Get state from service.
@@ -26,14 +28,16 @@ angular.module('angularjsOauthApp')
           OAuth.popup('github', {state: csrfToken}, function(error, result) {
 
             $http({
-              method: "JSONP",
-              url: "https://api.github.com/user",
+              method: 'JSONP',
+              url: 'https://api.github.com/user',
               params: {
                 access_token: result.access_token,
-                callback: "JSON_CALLBACK"
+                callback: 'JSON_CALLBACK'
               }
             })
             .success(function(data) {
+              scope.user.data = data;
+              scope.user.name = data.data.name
               loggedIn(data.data.name);
             }).
             error(function(data) {
